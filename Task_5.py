09b30b7b8b79247aecdc6d517a568e20a0396709
My Name <Me@gmail.com>
2022-09-23 04:15:54 +0400

    Task 5 finished

--- Task_5.py
+++ Task_5.py
@@ -5,3 +5,8 @@ for i in range(1):
     y1 = int(input("Введите Y для точки A: "))
     x2 = int(input("Введите X для точки B: "))
     y2 = int(input("Введите Y для точки B: "))
+num1 = (x2 - x1) ** 2
+num2 = (y2 - y1) ** 2
+sum = num1 + num2
+result = sum ** 0.5
+print(f"Расстояние между точками A и B = {round(result, 2)}")
